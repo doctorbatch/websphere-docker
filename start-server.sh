1c2fffde52a22f80047a7a104eba668a166be930
Timothy Fanelli <tim@zblservices.com>
2015-11-15 15:55:21 -0500

    cleaning up start-server

--- start-server.sh
+++ start-server.sh
@@ -11,11 +11,16 @@ function stopserver() {
   exit
 }
 
+function isRunning( ) {
+  return `(kill -s 0 $1 2>&1) > /dev/null`
+}
+
 rm $WAS_HOME/profiles/$PROFILE/logs/$SERVER/startServer.log
 $WAS_HOME/bin/startServer.sh $SERVER -profileName $PROFILE 2>&1
 PID_LINE=`grep 'ADMU3000I' $WAS_HOME/profiles/$PROFILE/logs/$SERVER/startServer.log`
 PID=`echo $PID_LINE | sed 's/..*process id is \([0-9][0-9]*\)$/\1/g'`
 
 echo Waiting on WebSphere Server Process $PID
-while `kill -s 0 $PID`; do sleep 5; done
+
+while isRunning $PID; do sleep 5; done
 echo "WebSphere Application Server process $PID has terminated. Goodbye."
