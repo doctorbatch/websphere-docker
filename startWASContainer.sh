ed4e424637c8ba78fecb827c8794a20dfa9ac204
Timothy C. Fanelli <tim@zblservices.com>
2015-11-19 15:35:18 -0500

    Added platform check for linux to startWASContainer -- no docker-machine on linux platforms

--- startWASContainer.sh
+++ startWASContainer.sh
@@ -19,11 +19,16 @@
 # Runs the websphere container identified by image $1 on docker-machine $2 (default by default).
 ###
 
-MACHINE=default
-if [ ! -z $2 ]; then
-  echo "Using machine $2"
-  MACHINE=$2
+PLATFORM=`uname`
+
+if [ "$PLATFORM" != "Linux" ]; then
+	MACHINE=default
+	if [ ! -z $2 ]; then
+	  echo "Using machine $2"
+	  MACHINE=$2
+	fi
+	
+	eval $(docker-machine env $MACHINE)
 fi
 
-eval $(docker-machine env $MACHINE)
 docker run -it -p 9080:9080 -p 9060:9060 -p 2809:2809 -p 9443:9443 -p 9043:9043 -p 8880:8880 $1
